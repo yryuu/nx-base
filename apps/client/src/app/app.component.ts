0e7b2bf1568bd4f5aa78084e593369218457338d
yryuu <yryuuyamazaki@gmail.com>
2021-03-27 16:41:55 +0900

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@nx-base/api-interfaces';
+
+@Component({
+  selector: 'nx-base-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
